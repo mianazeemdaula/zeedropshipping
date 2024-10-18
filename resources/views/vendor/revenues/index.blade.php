5ffe3ce0e901aa1431de70947df8e43f37314993
mianazeemdaula <mianazeemdaula@gmail.com>
2024-10-18 12:27:44 +0500

    xs

--- resources/views/vendor/revenues/index.blade.php
+++ resources/views/vendor/revenues/index.blade.php
@@ -1,86 +1,81 @@
 @extends('layouts.admin')
 
 @section('content')
-<section class="mx-auto w-full max-w-7xl px-4 py-4 min-h-screen">
-    <div class="flex flex-col space-y-4 md:flex-row md:items-center md:justify-between md:space-y-0">
-        <div class="flex items-center justify-between min-w-full">
-            <div>
-                <h2 class="text-lg font-semibold">Revenue</h2>
+    <section class="mx-auto w-full max-w-7xl px-4 py-4 min-h-screen">
+        <div class="flex flex-col space-y-4 md:flex-row md:items-center md:justify-between md:space-y-0">
+            <div class="flex items-center justify-between min-w-full">
+                <div>
+                    <h2 class="text-lg font-semibold">Revenue</h2>
+                </div>
+                <div>
+                    <span class="font-normal">Total Earn:</span>
+                    <span class="bg-primary-500 px-2 rounded-full text-white"> RS. {{ $total }}</span>
+                </div>
+                {{-- <a href="{{ route('vendor.bank-account.create') }}" class="px-5 text-white bg-black py-2 rounded-lg hover:bg-gray-800" >Create</a> --}}
             </div>
-            <div>
-                <span class="font-normal">Total Earn:</span>
-                <span class="bg-primary-500 px-2 rounded-full text-white"> RS. {{ $total }}</span>
-            </div>
-            {{-- <a href="{{ route('vendor.bank-account.create') }}" class="px-5 text-white bg-black py-2 rounded-lg hover:bg-gray-800" >Create</a> --}}
         </div>
-    </div>
-    <div class="mt-6 flex flex-col space-y-4">
-        <!-- Table Layout for Larger Screens -->
-        <div class="">
-            <div class="overflow-x-auto">
-                <div class="inline-block min-w-full align-middle">
-                    <div class="overflow-hidden border border-gray-200 md:rounded-lg">
-                        <table class="min-w-full divide-y divide-gray-200">
-                            <thead class="bg-gray-50">
-                                <tr>
-                                    <th scope="col" class="px-2 py-2 text-left text-xs font-normal text-gray-700 sm:px-4 sm:py-3.5">Order</th>
-                                    <th scope="col" class="px-2 py-2 text-left text-xs font-normal text-gray-700 sm:px-4 sm:py-3.5">Description</th>
-                                    <th scope="col" class="px-2 py-2 text-left text-xs font-normal text-gray-700 sm:px-4 sm:py-3.5">Paid At</th>
-                                    <th scope="col" class="px-2 py-2 text-left text-xs font-normal text-gray-700 sm:px-4 sm:py-3.5">Amount</th>
-                                    {{-- <th scope="col" class="px-2 py-2 text-left text-xs font-normal text-gray-700 sm:px-4 sm:py-3.5">Action</th> --}}
-                                </tr>
-                            </thead>
-                            <tbody class="divide-y divide-gray-200 bg-white">
-                                @if($revenues->isEmpty())
+        <div class="mt-6 flex flex-col space-y-4">
+            <!-- Table Layout for Larger Screens -->
+            <div class="">
+                <div class="overflow-x-auto">
+                    <div class="inline-block min-w-full align-middle">
+                        <div class="overflow-hidden border border-gray-200 md:rounded-lg">
+                            <table class="min-w-full divide-y divide-gray-200">
+                                <thead class="bg-gray-50">
                                     <tr>
-                                        <td class="whitespace nowrap px-2 py-4 text-sm sm:px-4 sm:py-4" colspan="9">No Earnings Yet</td>
+                                        <th scope="col"
+                                            class="px-2 py-2 text-left text-xs font-normal text-gray-700 sm:px-4 sm:py-3.5">
+                                            Order</th>
+                                        <th scope="col"
+                                            class="px-2 py-2 text-left text-xs font-normal text-gray-700 sm:px-4 sm:py-3.5">
+                                            Description</th>
+                                        <th scope="col"
+                                            class="px-2 py-2 text-left text-xs font-normal text-gray-700 sm:px-4 sm:py-3.5">
+                                            Credited On</th>
+                                        <th scope="col"
+                                            class="px-2 py-2 text-left text-xs font-normal text-gray-700 sm:px-4 sm:py-3.5">
+                                            Amount</th>
+                                        {{-- <th scope="col" class="px-2 py-2 text-left text-xs font-normal text-gray-700 sm:px-4 sm:py-3.5">Action</th> --}}
                                     </tr>
-                                @endif
-                                @foreach($revenues as $item)
-                                <tr>
-                                    <td class="whitespace-nowrap px-2 py-2 text-sm sm:px-4 sm:py-2">
-                                        <div class="font-medium">
-                                            {{ $item->order->id }}
-                                        </div>
-                                        <div>
-                                            {{ $item->order->order_number }}
-                                        </div>
-                                    </td>
-                                    <td class="whitespace-nowrap px-2 py-4 text-sm sm:px-4 sm:py-4">
-                                        {{ $item->description }}
-                                    </td>
-                                    <td class="whitespace nowrap px-2 py-4 text-sm sm:px-4 sm:py-4">
-                                        {{ $item->created_at }}
-                                    </td>
-                                    <td class="whitespace nowrap px-2 py-4 text-sm sm:px-4 sm:py-4">
-                                        {{ $item->amount }}
-                                    </td>
-                                    
-                                    {{-- <td class="whitespace-nowrap px-2 py-4 text-sm font-medium text-right sm:px-4 sm:py-4 flex">
-                                         <a href="#" class="text-gray-700 hover:text-blue-500">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0zm7.5 0c-.914 3.407-4.104 6-7.5 6s-6.586-2.593-7.5-6c.914-3.407 4.104-6 7.5-6s6.586 2.593 7.5 6z"/>
-                                        </svg>
-                                    </a>
-                                    <a href="{{ route('vendor.bank-account.edit', $item->id) }}">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1" class="w-5 h-5">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M11 19h-2a1 1 0 01-1-1v-2a1 1 0 01.293-.707l8.59-8.59a1.5 1.5 0 012.12 0l2.12 2.12a1.5 1.5 0 010 2.12l-8.59 8.59A1 1 0 0111 19z" />
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M13 17l-3-3m0 0l1-1m-1 1l-1-1m1 1h1.5m3-5.5L14 9m-1 1L11.5 7.5" />
-                                        </svg>
-                                    </a>
-                                    </td> --}}
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody class="divide-y divide-gray-200 bg-white text-xs">
+                                    @if ($revenues->isEmpty())
+                                        <tr>
+                                            <td class="whitespace nowrap px-2 py-4 sm:px-4 sm:py-4" colspan="9">
+                                                No Earnings Yet</td>
+                                        </tr>
+                                    @endif
+                                    @foreach ($revenues as $item)
+                                        <tr>
+                                            <td class="whitespace-nowrap px-2 py-1  sm:px-4 sm:py-2">
+                                                <div class="font-medium">
+                                                    {{ $item->order->id }}
+                                                </div>
+                                                <div>
+                                                    {{ $item->order->order_number }}
+                                                </div>
+                                            </td>
+                                            <td class="whitespace-nowrap px-2 py-1  sm:px-4 sm:py-1">
+                                                {{ $item->description }}
+                                            </td>
+                                            <td class="whitespace nowrap px-2 py-1 sm:px-4 sm:py-1">
+                                                {{ $item->created_at }}
+                                            </td>
+                                            <td class="whitespace nowrap px-2 py-1  sm:px-4 sm:py-1">
+                                                {{ $item->amount }}
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    {{-- <x-pagging :paginator=$products /> --}}
-    <div class="py-4">
-        {{ $revenues->links() }}
-    </div>
-</section>
+        {{-- <x-pagging :paginator=$products /> --}}
+        <div class="py-4">
+            {{ $revenues->links() }}
+        </div>
+    </section>
 @endsection
